3d4c919981117ca075d041ab464d8dcc4b92598c
Junyi Du <junyi_du@cs.ucsb.edu>
2019-11-13 16:52:24 -0800

    add javadoc and jacoco report

---
+++ docs/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"hello","l":"Application"},{"p":"hello","l":"AuthControllerAdvice"},{"p":"hello","l":"GithubOrgMembershipService"},{"p":"hello","l":"MembershipService"},{"p":"hello","l":"WebController"}]
