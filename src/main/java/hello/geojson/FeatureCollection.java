86c17e8592c326c4c38666460dbb19c7bc5c2320
Junyi Du <junyi_du@cs.ucsb.edu>
2019-11-17 15:30:16 -0800

    create object for the Metadata and display it

---
+++ src/main/java/hello/geojson/FeatureCollection.java
@@ -0,0 +1,44 @@
+package hello.geojson;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import hello.geojson.FeatureCollection;
+
+
+public class FeatureCollection {
+  public String type;
+
+  private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
+
+   /**
+     * Create a FeatureCollection object from json representation
+     * 
+     * @param json String of json returned by API endpoint {@code /classes/search}
+     * @return a new FeatureCollection object
+     * @see <a href=
+     *      "https://tools.ietf.org/html/rfc7946">https://tools.ietf.org/html/rfc7946</a>
+     */
+    public static FeatureCollection fromJSON(String json) {
+      try {
+          ObjectMapper objectMapper = new ObjectMapper();
+          objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+          FeatureCollection featureCollection = objectMapper.readValue(json, FeatureCollection.class);
+          return featureCollection;
+      } catch (JsonProcessingException jpe) {
+          logger.error("JsonProcessingException:" + jpe);
+          return null;
+      } catch (Exception e) {
+          logger.error("Exception:" + e);
+          return null;
+      }
+  }
+
+  public Metadata metadata;
+}
+
