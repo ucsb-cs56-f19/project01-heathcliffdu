800f42f364015c455ec43acf50c2b8cd32fddb02
Junyi Du <junyi_du@cs.ucsb.edu>
2019-11-16 17:57:28 -0800

    jd - Added placeholder EarthquakeQueryService and wired it up to results form

---
+++ src/main/java/hello/EarthquakeQueryService.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EarthquakeQueryService {
+
+    private Logger logger = LoggerFactory.getLogger(EarthquakeQueryService.class);
+
+    public String getJSON(int distance, int minmag) {
+        String fakeJson = "{ \"key\": \"value\" }";
+        String json = fakeJson;
+        logger.info("json=" + json);
+        return json;
+    }
+
+} 
