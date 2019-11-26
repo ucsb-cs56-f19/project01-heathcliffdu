a1e4ee4b78328dac4fb4041d8335ff8c792dbff1
Junyi Du <junyi_du@cs.ucsb.edu>
2019-11-26 15:01:38 -0800

    jd - update javadoc and jacoco report

--- docs/apidocs/package-search-index.js
+++ docs/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"hello"},{"l":"hello.geojson"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"hello"},{"l":"hello.controllers"},{"l":"hello.entities"},{"l":"hello.geojson"},{"l":"hello.repositories"}]
