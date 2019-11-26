0b77b3948328222146a94bf7416d2a5946349fd5
Junyi Du <junyi_du@cs.ucsb.edu>
2019-11-25 23:10:38 -0800

    jd - add users information to table

--- src/main/java/hello/AuthControllerAdvice.java
+++ src/main/java/hello/AuthControllerAdvice.java
@@ -6,8 +6,15 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
+import hello.repositories.UserRepository;
+import hello.entities.AppUser;
+import java.util.List;
+
+
 @ControllerAdvice
 public class AuthControllerAdvice {
+    @Autowired   
+    private UserRepository userRepository;
 
     @Autowired   
     private MembershipService membershipService;
@@ -20,13 +27,25 @@ public class AuthControllerAdvice {
     @ModelAttribute("id")
     public String getUid(OAuth2AuthenticationToken token){
         if (token == null) return "";
-        return token.getPrincipal().getAttributes().get("id").toString();
+
+        String uid = token.getPrincipal().getAttributes().get("id").toString();
+
+        List<AppUser> users = userRepository.findByUid(uid);
+
+        if (users.size()==0) {
+            AppUser u = new AppUser();
+            u.setUid(uid);
+            u.setLogin(token2login(token));
+            userRepository.save(u);
+        }
+
+        return uid;
     }
 
     @ModelAttribute("login")
     public String getLogin(OAuth2AuthenticationToken token){
         if (token == null) return "";
-        return token.getPrincipal().getAttributes().get("login").toString();
+        return token2login(token);
     }
 
     @ModelAttribute("isMember")
@@ -41,5 +60,9 @@ public class AuthControllerAdvice {
     @ModelAttribute("role")
     public String getRole(OAuth2AuthenticationToken token){
         return membershipService.role(token);
+    }
+
+    private String token2login(OAuth2AuthenticationToken token) {
+        return token.getPrincipal().getAttributes().get("login").toString();
     }
 }
