57e583361f5d42a750485469d25dc1663802ac6d
Junyi Du <junyi_du@cs.ucsb.edu>
2019-12-03 19:45:54 -0800

    jd - add a location search form to my app

---
+++ src/main/java/earthquakes/controllers/LocationController.java
@@ -0,0 +1,39 @@
+package earthquakes.controllers;
+
+import earthquakes.services.EarthquakeQueryService;
+import earthquakes.searches.LocSearch;
+
+
+import earthquakes.geojson.FeatureCollection;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.nimbusds.oauth2.sdk.client.ClientReadRequest;
+
+@Controller
+public class LocationController {
+
+
+    @GetMapping("/locations/search")
+    public String getLocationsSearch(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
+            LocSearch locSearch) {
+        return "locations/search";
+    }
+
+    @GetMapping("/locations/results")
+    public String getLocationResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
+            LocSearch locSearch) {
+        model.addAttribute("locSearch", locSearch);
+        return "locations/results";
+    }
+}
--- src/test/java/earthquakes/HomePageTest.java
+++ src/test/java/earthquakes/HomePageTest.java
@@ -77,8 +77,8 @@ public class HomePageTest {
     public void getHomePage_hasCorrectHeader() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
-                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").exists())
-                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").string("Earthquake Search"));
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[1]/a").exists())
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[1]/a").string("Earthquake Search"));
     }
 
     @Test
@@ -88,4 +88,12 @@ public class HomePageTest {
                 .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[3]/a").exists())
                 .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[3]/a").string("Users"));
     }
+
+    @Test
+ public void getHomePage_hasLocationLink() throws Exception {
+     mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
+             .andExpect(status().isOk())
+             .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").exists())
+             .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").string("Locations"));
+ }
 }
